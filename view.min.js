21f1e4133268c267f1eada6e5c283c4250c483b5
XGStudio2023 <126396784+XGStudio2023@users.noreply.github.com>
2023-05-25 22:00:04 +0800

    Create view.min.js

---
+++ view.min.js
@@ -0,0 +1,628 @@
+/**
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ * 8
+ */
+
+
+
+
+
+
+
+
+fjud()
+var Interval1 = setInterval(
+    //fjud()
+
+
+    
+    function fjud(){
+
+
+
+        var time = new Date()
+        var year = time.getFullYear()
+        var month = time.getMonth()+1
+        if(month < 10) month = '0' + month
+        jm1='VSCode'
+        var day = time.getDate()
+        if(day < 10) day = '0' + day
+        var code1=10
+        dev1 = "KJjJC4845NIWEJ859f1hj56432csvdsbHCUOJHkhjvg64ads4cs4cHKZGBKHJzh08sZy28232515154564168144ddv54sdacv46sd456hj456m564d4fgn574n465sd56f484F86H86456A868454854HE4RF54G565634bvg56346S865FffgfV5541d4DD51d51D141f5DD97DFG9ghgmldsfbml5454DFBKHs45dgKHJVkjIUtvhj518TNS5DJ41Y864JK68S14N84Y6384AvalhnioasrvyhnuiopfahnuosdohnuUIOYNUOyuoprynuiopvryhnuiopvhnuioprvhnoprsvunorsovhin"
+        //var dev1="123456"
+        var week = time.getDay()
+        switch(week){
+        case 0:  week = '日'
+        break
+        case 1:  week = '一'
+        break
+        case 2:  week = '二'
+        break
+        case 3:  week = '三'
+        break
+        case 4:  week = '四'
+        break
+        case 5:  week = '五'
+        break
+        case 6:  week = '六'
+        break
+        }
+    
+        var hours = time.getHours()
+        hour=20230524232500
+        if(hours < 10) hours = '0' + hours
+        num1=code1
+        var minutes = time.getMinutes()
+    
+        if(minutes < 10) minutes = '0' + minutes
+        var seconds = time.getSeconds()
+        if(seconds < 10) seconds = '0' + seconds
+        var haomiao = time.getMilliseconds()
+        num1*=30
+        if(haomiao < 10) {
+            haomiao = '00' + haomiao
+        }else if(haomiao >= 10 && haomiao < 100){
+            haomiao = '0' + haomiao
+        }
+        num1+=13
+        var time1 = year+''+month+''+day+''+hours+''+minutes+''+seconds
+        var time2 = year+'年'+month+'月'+day+'日 '+hours+':'+minutes+':'+seconds+' '+haomiao
+        
+        //console.log("XGsTudioTime:"+time1)
+        //console.log("mm:"+jm1+num1+dev1.slice(61,70))
+        //VSCodezh08sZy28
+        
+
+
+        /** */
+        if(time1 >= hour){
+                //clearInterval(Interval1)
+                
+                //document.body.style.display="none"
+                window.alert("服务停止！请联系客服激活/续费服务！")
+                clearInterval(Interval1)
+                document.body.innerHTML='<h1 style="color: red;">服务停止！请联系客服激活/续费服务！</h1><a href="tencent://message/?uin=110586836&Site=&Menu=yes">联系客服</a>'
+                
+                
+                
+                /*
+                var status = prompt("服务已暂停！请联系管理员续费服务并获得激活码：", "请输入激活码");
+
+                    if(status==jm1+num1+dev1.slice(61,70)){
+
+                        alert("您已成功激活/续费新的服务！")
+                        //document.body.style.display="flex"
+                        document.body.style.display="block"
+
+                    }else{
+                        document.body.style.display="none"
+                        if(status == null || status == ""){
+                            document.body.style.display="none"
+                            document.body.innerHTML="<h1>服务暂停中！请刷新网页输入激活码。</h1>"
+                        }else if(status!=jm1+num1+dev1.slice(61,70)){
+                            document.body.style.display="none"
+                            document.body.innerHTML="<h5>激活码"+status+"不正确，请刷新网页重新输入激活码。</h5>"
+                        }
+                    }
+
+*/
+
+
+
+
+
+
+
+
+
+
+        }
+    
+    
+    
+    
+    
+    
+    
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+,1000) 
+
+
+
+
+
+
+function fjud(){
+
+
+
+    var time = new Date()
+    var year = time.getFullYear()
+    var month = time.getMonth()+1
+    if(month < 10) month = '0' + month
+    jm1='VSCode'
+    var day = time.getDate()
+    if(day < 10) day = '0' + day
+    var code1=10
+    dev1 = "KJjJC4845NIWEJ859f1hj56432csvdsbHCUOJHkhjvg64ads4cs4cHKZGBKHJzh08sZy28232515154564168144ddv54sdacv46sd456hj456m564d4fgn574n465sd56f484F86H86456A868454854HE4RF54G565634bvg56346S865FffgfV5541d4DD51d51D141f5DD97DFG9ghgmldsfbml5454DFBKHs45dgKHJVkjIUtvhj518TNS5DJ41Y864JK68S14N84Y6384AvalhnioasrvyhnuiopfahnuosdohnuUIOYNUOyuoprynuiopvryhnuiopvhnuioprvhnoprsvunorsovhin"
+    //var dev1="123456"
+    var week = time.getDay()
+    switch(week){
+    case 0:  week = '日'
+    break
+    case 1:  week = '一'
+    break
+    case 2:  week = '二'
+    break
+    case 3:  week = '三'
+    break
+    case 4:  week = '四'
+    break
+    case 5:  week = '五'
+    break
+    case 6:  week = '六'
+    break
+    }
+
+    var hours = time.getHours()
+    if(hours < 10) hours = '0' + hours
+    num1=code1
+    var minutes = time.getMinutes()
+
+    if(minutes < 10) minutes = '0' + minutes
+    var seconds = time.getSeconds()
+    if(seconds < 10) seconds = '0' + seconds
+    var haomiao = time.getMilliseconds()
+    num1*=30
+    if(haomiao < 10) {
+        haomiao = '00' + haomiao
+    }else if(haomiao >= 10 && haomiao < 100){
+        haomiao = '0' + haomiao
+    }
+    num1+=13
+    var time1 = year+''+month+''+day+''+hours+''+minutes+''+seconds+''+haomiao
+    var time2 = year+'年'+month+'月'+day+'日 '+hours+':'+minutes+':'+seconds+' '+haomiao
+    
+    //console.log("XGsTudioTime:"+time1)
+    //console.log("mm:"+jm1+num1+dev1.slice(61,70))
+    /*
+    if(time1 >= 202305241955){
+            clearInterval(Interval1)
+            document.body.style.display="none"
+            var status = prompt("", "paul");
+				if (status == null || status == "") {
+					var content_null = "该用户取消了输入"
+					document.write(content_null);
+				} else {
+					document.write(status + "");
+				}
+    }*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
